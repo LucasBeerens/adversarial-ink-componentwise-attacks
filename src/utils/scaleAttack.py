466a76af944acc79231278c996d2aca3a1cb1906
LucasBeerens <lucasbeerens@gmail.com>
2022-07-07 17:03:18 +0100

    Add alg1_1 with clamp

--- src/utils/scaleAttack.py
+++ src/utils/scaleAttack.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-def scale(net, img, attack, cl, n=10):
+def scale(net, img, attack, cl, n=10, clamp=False):
     with torch.no_grad():
         imgLen = img.nelement()
         imShape = img.size()
@@ -11,7 +11,10 @@ def scale(net, img, attack, cl, n=10):
         for i in range(1000):
             a = i/1000
             xNew = x +  a * attack * ratio
-            newImg = torch.tensor(np.reshape(xNew, imShape)).float()
+            if clamp:
+                newImg = torch.clamp(torch.tensor(np.reshape(xNew, imShape)),0,1).float()
+            else:
+                newImg = torch.tensor(np.reshape(xNew, imShape)).float()
             outputs = net(newImg)
             _, predicted = torch.max(outputs.data,1)
             if predicted == cl:
