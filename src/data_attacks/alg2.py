d1f1839fb0d4e77d4d4e0a3746d52ba9cd2a7a1f
LucasBeerens <lucasbeerens@gmail.com>
2022-07-10 14:22:29 +0100

    Add iterative algorithms that keep images in bound

--- src/data_attacks/alg2.py
+++ src/data_attacks/alg2.py
@@ -58,7 +58,7 @@ class Al0():
                 newImVec = imVec + a * delx
                 newImg = np.reshape(newImVec,img.shape).float()
                 if self.disp:
-                    _, epss[i] = scale.scale(net,img,dx,cl,20)
+                    _, epss[i] = scale.specific(net,img,dx,cl,20)
                 
                 output = net(newImg)
                 _, predicted = torch.max(output.data,1)
@@ -71,3 +71,73 @@ class Al0():
         return scale.specific(net,img,dx,cl,20)
 
 
+class Al1():
+    def __init__(self,n=150,a=0.01,disp=False):
+        self.n = n
+        self.a = a
+        self.disp = disp
+
+    def __call__(self,net,img,cl):
+        n = self.n
+        a = self.a
+        with torch.no_grad():
+            net.eval()
+            output = net(img).numpy().transpose()
+            classCount = output.size
+            imgLen = img.nelement()
+            newImg = img
+            newImVec = np.reshape(newImg,(imgLen,1))
+            dx = np.zeros((imgLen,1))
+
+            c1 = np.hstack([np.eye(imgLen),np.zeros((imgLen,classCount))])
+            k1 = cp.Parameter((imgLen,1))
+            
+            G = np.array([[int(j==cl) for j in range(classCount)] for _ in range(classCount)])
+            c2 = np.hstack([np.zeros((classCount,imgLen)),np.eye(classCount) - G])
+            c21 = np.hstack([np.eye(imgLen), np.zeros((imgLen,classCount))])
+            c22 = np.hstack([-np.eye(imgLen), np.zeros((imgLen,classCount))])
+            c2 = np.vstack([c2,c21,c22])
+            k2 = cp.Parameter((2*imgLen + classCount,1))
+            
+            c3 = cp.Parameter((classCount,imgLen+classCount))
+            k3 = cp.Parameter((classCount,1))
+            
+            z = cp.Variable((imgLen + classCount,1))
+            obj = cp.Minimize(cp.sum_squares(c1 @ z - k1))
+            eqCons = [c2 @ z <= k2, c3 @ z == k3]
+            prob = cp.Problem(obj, eqCons)
+            assert prob.is_dcp(dpp=True)
+
+            epss = np.zeros(n)
+
+            for i in range(n):
+                jac = torch.autograd.functional.jacobian(net,newImg)
+                jac = jac.view(classCount, imgLen).numpy()
+
+                k1.value = -dx
+                k2.value = np.vstack([np.zeros((classCount,1)),(np.ones((imgLen,1)) - newImVec.numpy()),newImVec.numpy()])
+                k3.value = output
+                c3.value = np.hstack([-jac, np.eye(classCount)])
+
+                prob.solve(ignore_dpp = True)
+                
+                val = z.value
+
+                delx = val[:imgLen]
+                dx += a * delx
+
+                imVec = np.reshape(newImg,(imgLen,1))
+                newImVec = imVec + a * delx
+                newImg = np.reshape(newImVec,img.shape).float()
+                if self.disp:
+                    _, epss[i] = scale.specific(net,img,dx,cl,20)
+                
+                output = net(newImg)
+                _, predicted = torch.max(output.data,1)
+                if predicted == cl:
+                    break
+                output = output.numpy().transpose()
+            if self.disp:
+                plt.plot(epss)
+                plt.show()
+        return scale.specific(net,img,dx,cl,20)
--- src/data_attacks/alg3.py
+++ src/data_attacks/alg3.py
@@ -19,6 +19,9 @@ class Al0():
             classCount = output.size
             imgLen = img.nelement()
             newImg = img
+            imVec = np.reshape(newImg,(imgLen,1))
+            diagIm = np.diagflat(np.abs(imVec))
+
             v = np.zeros((imgLen,1))
             dx = np.zeros((imgLen,1))
 
@@ -46,9 +49,6 @@ class Al0():
             for i in range(n):
                 jac = torch.autograd.functional.jacobian(net,newImg)
                 jac = jac.view(classCount, imgLen)
-                imVec = np.reshape(newImg,(imgLen,1))
-                diagIm = np.diagflat(np.abs(imVec))
-                #diagIm = np.eye(imgLen)
 
                 k2.value = np.vstack([-v,v,np.zeros((classCount,1))])
                 k3.value = output
@@ -65,7 +65,82 @@ class Al0():
                 newImg = np.reshape(newImVec,img.shape).float()
                 #dx = (newImVec - np.reshape(img,(imgLen,1))).numpy()
                 if self.disp:
-                    _, epss[i] = scale.scale(net,img,dx,cl,20)
+                    _, epss[i] = scale.specific(net,img,dx,cl,20)
+                
+                output = net(newImg)
+                _, predicted = torch.max(output.data,1)
+                if predicted == cl:
+                    break
+                output = output.numpy().transpose()
+            if self.disp:
+                plt.plot(epss)
+                plt.show()
+        return scale.specific(net,img,dx,cl,20)
+
+class Al1():
+    def __init__(self,n=150,a=0.01,disp=False):
+        self.n = n
+        self.a = a
+        self.disp = disp
+
+    def __call__(self,net,img,cl):
+        n = self.n
+        a = self.a
+        with torch.no_grad():
+            net.eval()
+            output = net(img).numpy().transpose()
+            classCount = output.size
+            imgLen = img.nelement()
+            newImg = img
+            imVec = np.reshape(newImg,(imgLen,1)).numpy()
+            diagIm = np.diagflat(np.abs(imVec))
+
+            v = np.zeros((imgLen,1))
+            dx = np.zeros((imgLen,1))
+
+            c1 = np.hstack([np.ones((1,1)),np.zeros((1,classCount +  imgLen))])
+            k1 = np.zeros(1)
+            
+            G = np.array([[int(j==cl) for j in range(classCount)] for _ in range(classCount)])
+            c2 = np.hstack([np.zeros((classCount,1)),np.eye(classCount) - G,np.zeros((classCount,imgLen))])
+            c21 = np.hstack([-np.ones((imgLen,1)),np.zeros((imgLen,classCount)),np.eye(imgLen)])
+            c22 = np.hstack([-np.ones((imgLen,1)),np.zeros((imgLen,classCount)),-np.eye(imgLen)])
+            c23 = np.hstack([np.zeros((imgLen,classCount+1)),diagIm])
+            c24 = np.hstack([np.zeros((imgLen,classCount+1)),-diagIm])
+            c2 = np.vstack([c21,c22,c2,c23,c24])
+            k2 = cp.Parameter((4*imgLen+classCount,1))
+            
+            c3 = cp.Parameter((classCount,imgLen+classCount+1))
+            k3 = cp.Parameter((classCount,1))
+            
+            z = cp.Variable((1 + imgLen + classCount,1))
+            obj = cp.Minimize(cp.sum_squares(c1 @ z - k1))
+            eqCons = [c2 @ z <= k2, c3 @ z == k3]
+            prob = cp.Problem(obj, eqCons)
+            assert prob.is_dcp(dpp=True)
+
+            epss = np.zeros(n)
+
+            for i in range(n):
+                jac = torch.autograd.functional.jacobian(net,newImg)
+                jac = jac.view(classCount, imgLen)
+
+                k2.value = np.vstack([-v,v,np.zeros((classCount,1)),np.ones((imgLen,1))-imVec-diagIm@v,imVec+diagIm@v])
+                k3.value = output
+                c3.value = np.hstack([np.zeros((classCount,1)),np.eye(classCount),-jac@diagIm])
+
+                prob.solve(solver=cp.ECOS,ignore_dpp = True)
+                
+                val = z.value
+
+                dv = val[1+classCount:]
+                v += a * dv
+                dx = diagIm @ v
+                newImVec = np.reshape(img,(imgLen,1)) + dx
+                newImg = np.reshape(newImVec,img.shape).float()
+                #dx = (newImVec - np.reshape(img,(imgLen,1))).numpy()
+                if self.disp:
+                    _, epss[i] = scale.specific(net,img,dx,cl,20)
                 
                 output = net(newImg)
                 _, predicted = torch.max(output.data,1)
